75fb4c001fd415fe7397992b76b05c569e6480c2
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-06-02 17:44:07 -0300

    [feat] created component menu and style for mobile

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,20 @@
+import * as S from './style'
+
+export const Menu = () => {
+
+  return (
+    <S.Nav>
+      <S.UlPages>
+        <li>Sobre mim</li>
+        <li>Projetos</li>
+        <li>Habilidades</li>
+      </S.UlPages>
+      <S.Name>Felipe Cardoso</S.Name>
+      <S.UlSocialMedias>
+        <li>redes</li>
+        <li>redes</li>
+        <li>redes</li>
+      </S.UlSocialMedias>
+    </S.Nav>
+  )
+}
---
+++ src/components/Menu/style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+  background-color: #4d3e6b;
+  color: white;
+`
+
+export const UlPages = styled.ul`
+  margin: 0 auto;
+  display: flex;
+  list-style-type: none;
+  // border: 1px solid black;
+  max-width: 80%;
+  justify-content: space-between;
+  padding: 1.2em 1em 1.2em 1em;
+  // padding-right: 10px;
+  // margin-left: -30px;
+
+`
+
+export const Name = styled.div`
+  display: none;
+`
+
+export const UlSocialMedias = styled.ul`
+  display: none;
+  list-style-type: none;
+`
