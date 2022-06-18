3f8bb99698617bad04e83ad4af0f1f58ec164d3e
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-06-18 17:40:14 -0300

    [style] starting home style

--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components'
 
 export const Main = styled.main`
-  
+  padding-top: 10em;
+  background-color: #121212;
+  color: white;
+  text-align: center;
+
+  button {
+    color: white;
+    background-color: #121212;
+    border: 1px solid blue;
+  }
 `
